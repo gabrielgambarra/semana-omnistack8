13e1c71c9b91ea5322bfafd0933823f3f73ce3a2
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2019-08-09 18:45:32 -0400

    route to main and integration

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView, Image, StyleSheet, Text } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Main() {
     return (
-        <View />
+        <SafeAreaView style={styles.container}>
+            <Image source={logo} />
+
+            <View style={styles.cardsContainer}>
+                <View style={styles.card}>
+                    <Image source={{ uri: 'https://avatars1.githubusercontent.com/u/33934389?v=4' }} />
+                    <View style={styles.footer}>
+                        <Text style={styles.name}>Gabriel Gambarra</Text>
+                        <Text style={styles.bio}>Estudante de Ciência da Computação. Apaixonado por dar vida ao computador enquanto programa. Intelectual, filantropo... A parte do milionário ainda está vindo.</Text>
+
+                    </View>
+                </View>
+            </View>
+
+            <View />
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
+});
