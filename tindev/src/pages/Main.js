17519f9b1c50df95c71f38cc890bb2f599f180d0
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2019-08-12 16:59:27 -0400

    finish main screen

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import { View, SafeAreaView, Image, StyleSheet, Text } from 'react-native';
+import { View, SafeAreaView, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 import logo from '../assets/logo.png';
+import like from '../assets/like.png';
+import dislike from '../assets/dislike.png';
 
 export default function Main() {
     return (
         <SafeAreaView style={styles.container}>
-            <Image source={logo} />
+            <Image style={styles.logo} source={logo} />
 
             <View style={styles.cardsContainer}>
-                <View style={styles.card}>
-                    <Image source={{ uri: 'https://avatars1.githubusercontent.com/u/33934389?v=4' }} />
+                <View style={[styles.card, { zIndex: 3 }]}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/33934389?v=4' }} />
                     <View style={styles.footer}>
                         <Text style={styles.name}>Gabriel Gambarra</Text>
-                        <Text style={styles.bio}>Estudante de Ciência da Computação. Apaixonado por dar vida ao computador enquanto programa. Intelectual, filantropo... A parte do milionário ainda está vindo.</Text>
+                        <Text style={styles.bio} numberOfLines={3}>Estudante de Ciência da Computação. Apaixonado por dar vida ao computador enquanto programa. Intelectual, filantropo... A parte do milionário ainda está vindo.</Text>
+                    </View>
+                </View>
 
+                <View style={[styles.card, { zIndex: 2 }]}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/33934389?v=4' }} />
+                    <View style={styles.footer}>
+                        <Text style={styles.name}>Gabriel Gambarra</Text>
+                        <Text style={styles.bio} numberOfLines={3}>Estudante de Ciência da Computação. Apaixonado por dar vida ao computador enquanto programa. Intelectual, filantropo... A parte do milionário ainda está vindo.</Text>
+                    </View>
+                </View>
+
+                <View style={[styles.card, { zIndex: 1 }]}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/33934389?v=4' }} />
+                    <View style={styles.footer}>
+                        <Text style={styles.name}>Gabriel Gambarra</Text>
+                        <Text style={styles.bio} numberOfLines={3}>Estudante de Ciência da Computação. Apaixonado por dar vida ao computador enquanto programa. Intelectual, filantropo... A parte do milionário ainda está vindo.</Text>
                     </View>
                 </View>
             </View>
 
-            <View />
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Image source={dislike} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Image source={like} />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     );
 }
@@ -30,5 +54,65 @@ const styles = StyleSheet.create({
         backgroundColor: '#f5f5f5',
         alignItems: 'center',
         justifyContent: 'space-between'
+    },
+    logo: {
+        marginTop: 30
+    },
+    cardsContainer: {
+        flex: 1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        maxHeight: 500
+    },
+    card: {
+        borderWidth: 1,
+        borderColor: '#DDD',
+        borderRadius: 8,
+        margin: 30,
+        overflow: 'hidden',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+    },
+    avatar: {
+        flex: 1,
+        height: 300
+    },
+    footer: {
+        backgroundColor: '#FFF',
+        paddingHorizontal: 20,
+        paddingVertical: 15
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+    bio: {
+        fontSize: 14,
+        color: '#999',
+        marginTop: 5,
+        lineHeight: 18
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginBottom: 30
+    },
+    button: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#FFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        elevation: 2,
+        shadowOpacity: 0.05,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        }
     }
 });
