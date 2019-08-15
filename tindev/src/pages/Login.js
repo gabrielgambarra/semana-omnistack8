90f496f56a46b547d8699fb7b02abbc56bbee691
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2019-08-15 18:27:59 -0400

    connect mais with api

--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -18,13 +18,14 @@ export default function Login({ navigation }) {
     }, []);
 
     async function handleLogin() {
+        console.log(user)
         const response = await api.post('/devs', { username: user });
 
         const { _id } = response.data;
 
         await AsyncStorage.setItem('user', _id);
 
-        navigation.navigate('Main', { _id });
+        navigation.navigate('Main', { user: _id });
     }
     
   return (
