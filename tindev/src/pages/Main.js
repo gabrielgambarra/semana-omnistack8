367431aae2243a6d356c2d2ec9a0140dfe92b1bd
Gabriel Gambarra <gambarragabriel@gmail.com>
2019-09-24 21:42:44 -0400

    coompare error

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import AsyncStorage from '@react-native-community/async-storage';
 import { View, SafeAreaView, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
@@ -7,10 +8,12 @@ import api from '../services/api';
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ navigation }) {
     const id = navigation.getParam('user');
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUsers() {
@@ -24,6 +27,16 @@ export default function Main({ navigation }) {
         }
 
         loadUsers();
+    }, [id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:5000', {
+            query: { user: id }
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        })
     }, [id]);
 
     async function handleLike() {
@@ -58,20 +71,19 @@ export default function Main({ navigation }) {
                 <Image style={styles.logo} source={logo} />
             </TouchableOpacity>
 
-
             <View style={styles.cardsContainer}>
                 {users.length === 0
-                    ? <Text style={styles.empty}>Acabou :(</Text> :
-                    (
-                        users.map((user, index) => (
-                            <View key={user._id} style={[styles.card, { zIndex: users.length - index }]}>
-                                <Image style={styles.avatar} source={{ uri: user.avatar }} />
-                                <View style={styles.footer}>
-                                    <Text style={styles.name}>{user.name}</Text>
-                                    <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
+                    ? <Text style={styles.empty}>Acabou :(</Text>
+                        : (
+                            users.map((user, index) => (
+                                <View key={user._id} style={[styles.card, { zIndex: users.length - index }]}>
+                                    <Image style={styles.avatar} source={{ uri: user.avatar }} />
+                                    <View style={styles.footer}>
+                                        <Text style={styles.name}>{user.name}</Text>
+                                        <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
+                                    </View>
                                 </View>
-                            </View>
-                        ))
+                            ))
                     )}
             </View>
 
@@ -82,6 +94,20 @@ export default function Main({ navigation }) {
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.button} onPress={handleLike}>
                         <Image source={like} />
+                    </TouchableOpacity>
+                </View>
+            )}
+
+            {matchDev && (
+                <View style={styles.matchContainer}>
+                    <Image style={styles.matchImage} source={itsamatch} />
+                    <Image style={styles.matchAvatar} source={{ uri: matchDev.avatar }} />
+
+                    <Text style={styles.matchName}>{matchDev.name}</Text>
+                    <Text style={styles.matchBio}>{matchDev.bio}</Text>
+
+                    <TouchableOpacity onPress={() => setMatchDev(null)}>
+                        <Text style={styles.closeMatch}>Fechar</Text>
                     </TouchableOpacity>
                 </View>
             )}
@@ -163,5 +189,48 @@ const styles = StyleSheet.create({
             width: 0,
             height: 2
         }
+    },
+    matchContainer: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    matchImage: {
+        height: 60,
+        resizeMode: 'contain'
+    },
+
+    matchAvatar: {
+        width: 160,
+        height: 160,
+        borderRadius: 80,
+        borderWidth: 5,
+        borderColor: '#FFF',
+        marginVertical: 30,
+    },
+
+    matchName: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#FFF'
+    },
+
+    matchBio: {
+        marginTop: 10,
+        fontSize: 16,
+        color: 'rgba(255, 255, 255, 0.8)',
+        lineHeight: 24,
+        textAlign: 'center',
+        paddingHorizontal: 30
+    },
+
+    closeMatch: {
+        fontSize: 16,
+        color: 'rgba(255, 255, 255, 0.8)',
+        textAlign: 'center',
+        marginTop: 30,
+        fontWeight: 'bold'
     }
 });
